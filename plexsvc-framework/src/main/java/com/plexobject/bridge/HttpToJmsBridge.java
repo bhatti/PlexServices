bbccec2827edba94d05aa6d34574c0cc205feb53
Shahzad Bhatti <sbhatti@peak6.come>
2014-08-04 15:09:18 -0700

    adding status for http response

--- plexsvc-framework/src/main/java/com/plexobject/bridge/HttpToJmsBridge.java
+++ plexsvc-framework/src/main/java/com/plexobject/bridge/HttpToJmsBridge.java
@@ -31,125 +31,120 @@ import com.plexobject.util.IOUtils;
 
 // See http://git.eclipse.org/c/jetty/org.eclipse.jetty.project.git/tree/examples/async-rest/async-rest-jar/src/main/java/org/eclipse/jetty/example/asyncrest/AsyncRestServlet.java?h=release-9
 public class HttpToJmsBridge extends AbstractHandler {
-	private static final Logger log = LoggerFactory
-			.getLogger(HttpToJmsBridge.class);
-	private final HttpServer httpServer;
-	private final JmsClient jmsClient;
-	private final Map<Method, PathsLookup<HttpToJmsEntry>> entriesPathsByMethod = new ConcurrentHashMap<>();
+    private static final Logger log = LoggerFactory
+            .getLogger(HttpToJmsBridge.class);
+    private final HttpServer httpServer;
+    private final JmsClient jmsClient;
+    private final Map<Method, PathsLookup<HttpToJmsEntry>> entriesPathsByMethod = new ConcurrentHashMap<>();
 
-	public HttpToJmsBridge(Configuration config,
-			Collection<HttpToJmsEntry> entries) {
-		this.httpServer = new HttpServer(config, this);
-		this.jmsClient = new JmsClient(config);
+    public HttpToJmsBridge(Configuration config,
+            Collection<HttpToJmsEntry> entries) {
+        this.httpServer = new HttpServer(config, this);
+        this.jmsClient = new JmsClient(config);
 
-		for (HttpToJmsEntry e : entries) {
-			PathsLookup<HttpToJmsEntry> entryPaths = entriesPathsByMethod.get(e
-					.getMethod());
-			if (entryPaths == null) {
-				entryPaths = new PathsLookup<HttpToJmsEntry>();
-				entriesPathsByMethod.put(e.getMethod(), entryPaths);
-			}
-			if (entryPaths.get(e.getPath(), new HashMap<String, Object>()) != null) {
-				throw new IllegalStateException(
-						"Mapping is already registered for " + e);
-			}
-			entryPaths.put(e.getPath(), e);
-			log.info("Adding HTTP->JMS mapping for " + e.getShortString());
-		}
-	}
+        for (HttpToJmsEntry e : entries) {
+            PathsLookup<HttpToJmsEntry> entryPaths = entriesPathsByMethod.get(e
+                    .getMethod());
+            if (entryPaths == null) {
+                entryPaths = new PathsLookup<HttpToJmsEntry>();
+                entriesPathsByMethod.put(e.getMethod(), entryPaths);
+            }
+            if (entryPaths.get(e.getPath(), new HashMap<String, Object>()) != null) {
+                throw new IllegalStateException(
+                        "Mapping is already registered for " + e);
+            }
+            entryPaths.put(e.getPath(), e);
+            log.info("Adding HTTP->JMS mapping for " + e.getShortString());
+        }
+    }
 
-	public void startBridge() {
-		jmsClient.start();
-		httpServer.start();
-	}
+    public void startBridge() {
+        jmsClient.start();
+        httpServer.start();
+    }
 
-	public void stopBridge() {
-		jmsClient.stop();
-		httpServer.stop();
-	}
+    public void stopBridge() {
+        jmsClient.stop();
+        httpServer.stop();
+    }
 
-	@Override
-	public void handle(final String target, final Request baseRequest,
-			final HttpServletRequest request, final HttpServletResponse response)
-			throws IOException, ServletException {
+    @Override
+    public void handle(final String target, final Request baseRequest,
+            final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException, ServletException {
 
-		Map<String, Object> params = HttpServer.getParams(baseRequest);
+        Map<String, Object> params = HttpServer.getParams(baseRequest);
 
-		PathsLookup<HttpToJmsEntry> entryPaths = entriesPathsByMethod
-				.get(Method.valueOf(baseRequest.getMethod()));
-		final HttpToJmsEntry entry = entryPaths != null ? entryPaths.get(
-				baseRequest.getPathInfo(), params) : null;
+        PathsLookup<HttpToJmsEntry> entryPaths = entriesPathsByMethod
+                .get(Method.valueOf(baseRequest.getMethod()));
+        final HttpToJmsEntry entry = entryPaths != null ? entryPaths.get(
+                baseRequest.getPathInfo(), params) : null;
 
-		if (entry == null) {
-			log.warn("Unknown request received " + baseRequest.getMethod()
-					+ " at " + baseRequest.getPathInfo() + " -> "
-					+ HttpServer.getParams(baseRequest));
-			return;
-		}
-		final String text = IOUtils.toString(baseRequest.getInputStream());
-		// log.debug("Forwarding " + entry + ", text " + text + ", params "
-		// + params);
-		final AsyncContext async = request.startAsync();
-		async.setTimeout(entry.getTimeoutSecs() * 1000);
-		try {
-			jmsClient.sendReceive(entry.getDestination(), params, text,
-					new Handler<Response>() {
-						@Override
-						public void handle(Response reply) {
-							try {
-								// async.dispatch();
-								response.setContentType(entry.getContentType());
-								String status = reply.getProperty("status");
-								if (status != null) {
-									try {
-										response.setStatus(Integer
-												.parseInt(status));
-									} catch (NumberFormatException e) {
-									}
-								}
-								for (String name : reply.getPropertyNames()) {
-									Object value = reply.getProperty(name);
-									if (value != null) {
-										if (value instanceof String) {
-											response.addHeader(name,
-													(String) value);
-										} else {
-											response.addHeader(name,
-													value.toString());
-										}
-									}
-								}
-								response.getWriter().println(
-										(String) reply.getPayload());
-								baseRequest.setHandled(true);
-								async.complete();
+        if (entry == null) {
+            log.warn("Unknown request received " + baseRequest.getMethod()
+                    + " at " + baseRequest.getPathInfo() + " -> "
+                    + HttpServer.getParams(baseRequest));
+            return;
+        }
+        final String text = IOUtils.toString(baseRequest.getInputStream());
+        // log.debug("Forwarding " + entry + ", text " + text + ", params "
+        // + params);
+        final AsyncContext async = request.startAsync();
+        async.setTimeout(entry.getTimeoutSecs() * 1000);
+        try {
+            jmsClient.sendReceive(entry.getDestination(), params, text,
+                    new Handler<Response>() {
+                        @Override
+                        public void handle(Response reply) {
+                            try {
+                                // async.dispatch();
+                                response.setContentType(entry.getContentType());
+                                if (response.getStatus() > 0) {
+                                    response.setStatus(response.getStatus());
+                                }
+                                for (String name : reply.getPropertyNames()) {
+                                    Object value = reply.getProperty(name);
+                                    if (value != null) {
+                                        if (value instanceof String) {
+                                            response.addHeader(name,
+                                                    (String) value);
+                                        } else {
+                                            response.addHeader(name,
+                                                    value.toString());
+                                        }
+                                    }
+                                }
+                                response.getWriter().println(
+                                        (String) reply.getPayload());
+                                baseRequest.setHandled(true);
+                                async.complete();
 
-								log.info("Replying back " + entry + ", reply "
-										+ reply);
-							} catch (Exception e) {
-								log.error("Could not send back " + reply, e);
-							}
-						}
-					});
-		} catch (Exception e) {
-			log.error("Failed to send request", e);
-		}
-	}
+                                log.info("Replying back " + entry + ", reply "
+                                        + reply);
+                            } catch (Exception e) {
+                                log.error("Could not send back " + reply, e);
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            log.error("Failed to send request", e);
+        }
+    }
 
-	public static void main(String[] args) throws Exception {
-		if (args.length != 2) {
-			System.err.println("Usage: java " + HttpToJmsBridge.class.getName()
-					+ " properties-file mapping-json-file");
-			System.exit(1);
-		}
-		final String mappingJson = IOUtils
-				.toString(new FileInputStream(args[1]));
-		Collection<HttpToJmsEntry> entries = new JsonObjectCodec().decode(
-				mappingJson, new TypeReference<List<HttpToJmsEntry>>() {
-				});
-		HttpToJmsBridge bridge = new HttpToJmsBridge(
-				new Configuration(args[0]), entries);
-		bridge.startBridge();
-		Thread.currentThread().join();
-	}
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: java " + HttpToJmsBridge.class.getName()
+                    + " properties-file mapping-json-file");
+            System.exit(1);
+        }
+        final String mappingJson = IOUtils
+                .toString(new FileInputStream(args[1]));
+        Collection<HttpToJmsEntry> entries = new JsonObjectCodec().decode(
+                mappingJson, new TypeReference<List<HttpToJmsEntry>>() {
+                });
+        HttpToJmsBridge bridge = new HttpToJmsBridge(
+                new Configuration(args[0]), entries);
+        bridge.startBridge();
+        Thread.currentThread().join();
+    }
 }
--- plexsvc-framework/src/main/java/com/plexobject/service/jetty/HttpResponseBuilder.java
+++ plexsvc-framework/src/main/java/com/plexobject/service/jetty/HttpResponseBuilder.java
@@ -17,75 +17,77 @@ import com.plexobject.service.ErrorResponse;
 import com.plexobject.service.ServiceConfig;
 
 public class HttpResponseBuilder extends ResponseBuilder {
-	private static final Logger log = LoggerFactory
-			.getLogger(HttpResponseBuilder.class);
+    private static final Logger log = LoggerFactory
+            .getLogger(HttpResponseBuilder.class);
 
-	private final ServiceConfig config;
-	private final Request baseRequest;
-	private final HttpServletResponse response;
+    private final ServiceConfig config;
+    private final Request baseRequest;
+    private final HttpServletResponse response;
 
-	public HttpResponseBuilder(final ServiceConfig config,
-			final Request baseRequest, final HttpServletResponse response) {
-		this.config = config;
-		this.baseRequest = baseRequest;
-		this.response = response;
-	}
+    public HttpResponseBuilder(final ServiceConfig config,
+            final Request baseRequest, final HttpServletResponse response) {
+        this.config = config;
+        this.baseRequest = baseRequest;
+        this.response = response;
+    }
 
-	public void addSessionId(String value) {
-		response.addCookie(new Cookie(SessionValidator.SESSION_ID, value));
-	}
+    public void addSessionId(String value) {
+        response.addCookie(new Cookie(SessionValidator.SESSION_ID, value));
+    }
 
-	public void redirect(String location) {
-		try {
-			response.sendRedirect(location);
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to redirect to " + location);
-		}
-	}
+    public void redirect(String location) {
+        try {
+            response.sendRedirect(location);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to redirect to " + location);
+        }
+    }
 
-	public final void sendSuccess(Object payload) {
-		try {
-			response.setContentType(config.contentType());
-			response.setStatus(status);
-			for (Map.Entry<String, Object> e : properties.entrySet()) {
-				response.addHeader(e.getKey(), e.getValue().toString());
-			}
-			baseRequest.setHandled(true);
-			String replyText = ObjectCodeFactory.getObjectCodec(config.codec())
-					.encode(payload);
-			response.getWriter().println(replyText);
-		} catch (Exception e) {
-			log.error("Failed to write " + payload + ", " + this, e);
-		}
-	}
+    public final void sendSuccess(Object payload) {
+        try {
+            response.setContentType(config.contentType());
+            if (response.getStatus() > 0) {
+                response.setStatus(response.getStatus());
+            }
+            for (Map.Entry<String, Object> e : properties.entrySet()) {
+                response.addHeader(e.getKey(), e.getValue().toString());
+            }
+            baseRequest.setHandled(true);
+            String replyText = ObjectCodeFactory.getObjectCodec(config.codec())
+                    .encode(payload);
+            response.getWriter().println(replyText);
+        } catch (Exception e) {
+            log.error("Failed to write " + payload + ", " + this, e);
+        }
+    }
 
-	@Override
-	public void sendError(Exception e) {
-		response.setContentType(config.contentType());
-		response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		baseRequest.setHandled(true);
-		try {
-			String replyText = ObjectCodeFactory.getObjectCodec(config.codec())
-					.encode(new ErrorResponse(e));
-			response.getWriter().println(replyText);
-		} catch (IOException ex) {
-			log.error("Failed to send error " + e + ", " + this, ex);
-		}
-	}
+    @Override
+    public void sendError(Exception e) {
+        response.setContentType(config.contentType());
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        baseRequest.setHandled(true);
+        try {
+            String replyText = ObjectCodeFactory.getObjectCodec(config.codec())
+                    .encode(new ErrorResponse(e));
+            response.getWriter().println(replyText);
+        } catch (IOException ex) {
+            log.error("Failed to send error " + e + ", " + this, ex);
+        }
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Method:" + baseRequest.getMethod());
-		sb.append(", Path:" + baseRequest.getPathInfo());
-		sb.append(", Host:"
-				+ baseRequest.getRemoteInetSocketAddress().getHostName());
-		for (Map.Entry<String, String[]> e : baseRequest.getParameterMap()
-				.entrySet()) {
-			sb.append(", " + e.getKey() + " -> " + e.getValue()[0]);
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Method:" + baseRequest.getMethod());
+        sb.append(", Path:" + baseRequest.getPathInfo());
+        sb.append(", Host:"
+                + baseRequest.getRemoteInetSocketAddress().getHostName());
+        for (Map.Entry<String, String[]> e : baseRequest.getParameterMap()
+                .entrySet()) {
+            sb.append(", " + e.getKey() + " -> " + e.getValue()[0]);
 
-		}
-		return sb.toString();
-	}
+        }
+        return sb.toString();
+    }
 
 }
