fc15873a5b7e88306a21125b75c4bb39539ee7bf
Shahzad Bhatti <sbhatti@peak6.come>
2014-11-17 12:19:15 -0800

    added jmx options to ping startup script

--- plexsvc-samples/ping.sh
+++ plexsvc-samples/ping.sh
@@ -6,8 +6,9 @@ do
 done
 export CLASSPATH 
 #java -Dlog4j.configuration=log4j.xml com.plexobject.ping.Main ping.properties web 
-java -Dlog4j.configuration=log4j.xml com.plexobject.ping.Main ping.properties jms
+java -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=9192 -Dlog4j.configuration=log4j.xml com.plexobject.ping.Main ping.properties jms
   
+    
 #openssl genrsa -des3 -out server.key 1024
 #cp server.key server.key.org
 #openssl rsa -in server.key.org -out server.key
