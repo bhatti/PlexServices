1fd9a92b24340e551b4daf84bea51e6306f41d05
Shahzad Bhatti <shahzad.bhatti@trademonstergroup.com>
2015-03-05 18:24:26 -0800

    minor refactoring

--- plexsvc-framework/src/main/java/com/plexobject/service/ServiceRegistry.java
+++ plexsvc-framework/src/main/java/com/plexobject/service/ServiceRegistry.java
@@ -246,17 +246,18 @@ public class ServiceRegistry implements ServiceContainer, ServiceRegistryMBean {
 
     @Override
     public synchronized void start() {
-        log.info("starting containers " + _containers.size());
+        if (serviceRegistryLifecycleAware != null) {
+            log.info("invoking onStarted for " + serviceRegistryLifecycleAware + " ...");
+            serviceRegistryLifecycleAware.onStarted(this);
+        }
+        //
+        log.info("starting containers size " + _containers.size());
         for (ServiceContainer g : _containers.values()) {
             if (g.getHandlers().size() > 0) {
                 g.start();
             }
         }
         running = true;
-        if (serviceRegistryLifecycleAware != null) {
-            log.info("invoking onStarted...");
-            serviceRegistryLifecycleAware.onStarted(this);
-        }
     }
 
     @Override
