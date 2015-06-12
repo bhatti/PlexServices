a6b70e71b57628905fd7425d5bf583c329f8eaab
Shahzad Bhatti <shahzad.bhatti@trademonstergroup.com>
2015-06-11 19:51:28 -0700

    added test class

---
+++ plexsvc-framework/src/test/java/com/plexobject/handler/javaws/Customer.java
@@ -0,0 +1,65 @@
+package com.plexobject.handler.javaws;
+
+public class Customer {
+    private String accountNumber;
+    private Boolean streaming;
+
+    public Customer() {
+
+    }
+
+    public Customer(String accountNumber, Boolean streaming) {
+        this.accountNumber = accountNumber;
+        this.streaming = streaming;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Boolean getStreaming() {
+        return streaming;
+    }
+
+    public void setStreaming(Boolean streaming) {
+        this.streaming = streaming;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((accountNumber == null) ? 0 : accountNumber.hashCode());
+        result = prime * result
+                + ((streaming == null) ? 0 : streaming.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Customer other = (Customer) obj;
+        if (accountNumber == null) {
+            if (other.accountNumber != null)
+                return false;
+        } else if (!accountNumber.equals(other.accountNumber))
+            return false;
+        if (streaming == null) {
+            if (other.streaming != null)
+                return false;
+        } else if (!streaming.equals(other.streaming))
+            return false;
+        return true;
+    }
+
+}
